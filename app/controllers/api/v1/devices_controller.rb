512d7f0c785c675e34bf097873cf62f21d7fa016
Hien Vo <hien.vo@mingle.com>
2015-11-24 02:52:40 +0700

    [c] api for register device

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::DevicesController < Api::V1::BaseApiController
+  before_filter :basic_authenticate, only: [:register]
 
   def register
     identifier = params[:device][:identifier]
@@ -8,5 +9,17 @@ class Api::V1::DevicesController < Api::V1::BaseApiController
       device = Device.create(device_params)
     end
     render json: { device_id: device.id }
+  end
+
+  private
+  def device_params
+    params.require(:device).permit(:id, :push_notification_token, :type, :identifier, :model, :os_version, :app_version)
+  end
+
+  protected
+  def basic_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTHENTICATION_USERNAME'] && password == ENV['BASIC_AUTHENTICATION_PASSWORD']
+    end
   end
 end
