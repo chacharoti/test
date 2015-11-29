bc9483d18d5079447deac9ea1190407755ddf7a8
Hien Vo <hien.vo@mingle.com>
2015-11-29 15:19:19 +0700

    [f] add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :basic_authenticate
+
+  def basic_authenticate
+    web_username = ENV['WEB_USERNAME']
+    web_password = ENV['WEB_PASSWORD']
+    if web_username.present? && web_password.present?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == web_username && password == web_password
+      end
+    end
+  end
 end
