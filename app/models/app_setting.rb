35aeba46a47d04e05f2fd82e552deac9397ef2b6
Hien Vo <hien.vo@mingle.com>
2015-12-01 23:53:42 +0700

    [b] app setting not return correctly

--- app/models/app_setting.rb
+++ app/models/app_setting.rb
@@ -2,7 +2,9 @@ class AppSetting < ActiveRecord::Base
   def self.public_items_for_user user
     items = {}
     self.where(is_public: 1).each do |app_setting|
-      items[app_setting.key] = app_setting.value.gsub!('{{user_unique_identifier}}', user.unique_identifier)
+      value = app_setting.value
+      value.gsub!('{{user_unique_identifier}}', user.unique_identifier)
+      items[app_setting.key] = value
     end
     items
   end
