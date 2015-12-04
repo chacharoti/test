b568c8e271962a081fa13aec8fd901caeaf841d9
Hien Vo <hien.vo@mingle.com>
2015-12-04 13:54:32 +0700

    [c] update doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -9,8 +9,25 @@ Doorkeeper.configure do
   end
 
   resource_owner_from_credentials do |routes|
-    user = User.find_by(email: params[:username]) || User.find_by(fb_user_id: params[:username])
-    (user.present? && (user.valid_password?(params[:password]) || user.fb_access_token == params[:password])) ? user : redirect_to(new_user_session_url)
+    if (username = params[:username]).present? && (password = params[:password]).present?
+      if user = User.find_by(email: username)
+        if user.valid_password?(password)
+          user
+        else
+          redirect_to('/')
+        end
+      elsif user = User.find_by(fb_user_id: username)
+        if user.fb_acress_token == password
+          user
+        else
+          redirect_to('/')
+        end
+      else
+        redirect_to('/')
+      end
+    else
+      redirect_to('/')
+    end
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
