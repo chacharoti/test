e552ef73ea7e2ce9c6b93311e86fa96c7ad8f544
chacharoti <chacharoti@gmail.com>
2016-01-06 23:19:25 +0700

    Remove background color of section

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -3,7 +3,6 @@ var set_up_full_page_scroll = function() {
     'verticalCentered': false,
     'css3': true,
 	  'scrollingSpeed': 700,
-    'sectionsColor': ['#1fb287', '#f8f8f8', '#fff', '#fff', '#F0F2F4', '#fff', '#F0F2F4'],
     'navigation': true,
     'navigationPosition': 'right'
   });
