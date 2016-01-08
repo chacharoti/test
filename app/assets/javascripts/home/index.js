e250900f94b109e3454eeaa01da50bfed15d820b
chacharoti <chacharoti@gmail.com>
2016-01-08 15:09:55 +0700

    Restructue js

--- app/assets/javascripts/home/index.js
+++ app/assets/javascripts/home/index.js
@@ -1,9 +1,13 @@
-var set_up_full_page_scroll = function() {
-  $('#landing_page').fullpage({
-    'verticalCentered': false,
-    'css3': true,
-	  'scrollingSpeed': 700,
-    'navigation': true,
-    'navigationPosition': 'right'
-  });
-}
+var HomeIndexJs = {};
+
+(function() {
+	var set_up_full_page_scroll = HomeIndexJs.set_up_full_page_scroll = function(content) {
+		$('#landing_page').fullpage({
+			'verticalCentered': false,
+			'css3': true,
+			'scrollingSpeed': 700,
+			'navigation': true,
+			'navigationPosition': 'right'
+		});
+	}
+} ());
--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -5,6 +5,6 @@ var HomeMainJs = {};
 (function() {
   var init = HomeMainJs.init = function(options) {
     HomeMainJs.options = options;
-    set_up_full_page_scroll();
+	  HomeIndexJs.set_up_full_page_scroll();
   }
 } ());
