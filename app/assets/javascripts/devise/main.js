7c822eae00d4d128c552d5f48ce1d45f5a317233
chacharoti <chacharoti@gmail.com>
2016-02-13 15:18:48 +0700

    Add validation for sign up 1st step

--- app/assets/javascripts/devise/main.js
+++ app/assets/javascripts/devise/main.js
@@ -6,6 +6,19 @@ var DevisedMainJs = {};
   }
 
   var validate_sign_up_first_step = DevisedMainJs.validate_sign_up_first_step = function() {
+  	$("#new_user").validate();
+
+		$( "#user_email_confirmation" ).rules( "add", {
+	    equalTo: "#user_email",
+	    messages: {
+		    equalTo: "Your email is not match. Please try again."
+		  }
+	  });
+
+	  $( "#user_password" ).rules( "add", {
+	    minlength: 6
+	  });
+
   	$('#move-second-step').click(function() {
       $("#new_user").valid();
   	});
