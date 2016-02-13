67aee82b5ee15ca73b61f6df8e4b49e81cce3a22
chacharoti <chacharoti@gmail.com>
2016-02-13 14:27:15 +0700

    Set up validation for sign-in page

--- app/assets/javascripts/shared/main.js
+++ app/assets/javascripts/shared/main.js
@@ -12,4 +12,10 @@ var SharedMainJs = {};
   var set_full_page = SharedMainJs.set_full_page = function(page_id) {
     SharedFullPageJS.init_full_page(page_id);
   }
+
+  var init_validate_for = SharedMainJs.init_validate_for = function(button_id, form_id) {
+  	$('#' + button_id ).click(function() {
+      $("#" + form_id ).valid();
+  	});
+  }
 } ());
