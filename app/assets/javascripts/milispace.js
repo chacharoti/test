c36a7fb84340520563db2dd4e103df0e5938e0a6
chacharoti <chacharoti@gmail.com>
2016-02-21 15:12:11 +0700

    Add loading state

--- app/assets/javascripts/milispace.js
+++ app/assets/javascripts/milispace.js
@@ -2,6 +2,10 @@
 //= require shared/main
 //= require devise/main
 
+jQuery(window).load(function() {
+  jQuery("#loading-page").fadeOut("slow");
+})
+
 $( document ).ready(function() {
   new WOW().init();
   SharedMainJs.init();
